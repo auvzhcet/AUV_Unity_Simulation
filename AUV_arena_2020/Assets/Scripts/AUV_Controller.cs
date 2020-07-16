6f69fbe0c29841532a8bd812306ad63c11d862c7
syedjawadakhtar <syedjawadakhtar@gmail.com>
2020-07-16 14:49:02 +0530

    Initial Commit

---
+++ AUV_arena_2020/Assets/Scripts/AUV_Controller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class AUV_Controller : MonoBehaviour
+{
+    public float speed = 50;
+    public float torque = 10;
+    //public GameObject Left_thruster;
+    //public GameObject Right_thruster;
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Left_thruster.AddForce(transform.forward * 500);
+        float moveHorizantal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        
+        //Freeze rotation 
+        rb.freezeRotation = true;
+        //Rotating Player
+        //transform.Rotate(0, moveHorizantal, 0);
+
+        Vector3 movement = new Vector3(moveHorizantal, 0, moveVertical);
+        rb.AddForce(movement * speed );
+
+        //rb.AddTorque(transform.up * moveHorizantal);
+        //if(Input.GetKey(KeyCode.Q))
+        //    rb.AddTorque(Vector3.right * speed * Time.deltaTime);
+        //if (Input.GetKey(KeyCode.E))
+        //    rb.AddTorque(-Vector3.right * speed * Time.deltaTime);
+
+
+    }
+}
---
+++ AUV_arena_2020/README.md
@@ -0,0 +1,5 @@
+# Singapore AUV Challenge 2020
+
+This arena has the Qualification round and Main arena round props, according to the dimensions provided by the SAUVC 2020 [rulebook](www.sauvc.org/rulebook). It also consists an AUV with 4 thrusters and two cameras attached to it.
+
+There is only 4 movements to the vehcle: forward, backward, slide left and right.
